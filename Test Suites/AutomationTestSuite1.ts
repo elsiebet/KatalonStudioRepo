e22792c440823532a48f72df08aaded919f1ad84
liznthenya <liznthenya@users.noreply.github.com>
2018-01-08 19:07:03 +0300

    Add NewChanges

--- Test Suites/AutomationTestSuite1.ts
+++ Test Suites/AutomationTestSuite1.ts
@@ -4,7 +4,7 @@
    <name>AutomationTestSuite1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-01-08T18:59:29</lastRun>
+   <lastRun>2018-01-08T19:04:28</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
